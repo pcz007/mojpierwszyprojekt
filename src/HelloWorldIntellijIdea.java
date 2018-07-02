d0207104d7ee31680f7e08ede69ec3af810a07cc
PC <pcz@outlook.com>
2018-07-02 18:59:18 +0200

    Dodanie kolejnej linijki cudownie!

--- src/HelloWorldIntellijIdea.java
+++ src/HelloWorldIntellijIdea.java
@@ -3,5 +3,8 @@ public class HelloWorldIntellijIdea {
     public static void main(String[] args) {
         System.out.println("Komitowanie");
 
+        
+        System.out.println("Komitowanie działa cudownie");
+
     }
 }
